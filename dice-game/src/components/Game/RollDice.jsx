f18f11a179d254812c9ca59555ab01aa7e1a97ef
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-29 18:18:57 +0545

    Random Image Generator Used

--- dice-game/src/components/Game/RollDice.jsx
+++ dice-game/src/components/Game/RollDice.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const RollDice = () => {
-  return (
-    <div>
-      <img src="images/dice_1.png" alt="Dice 1" srcset="" />
+  const [currentDice, setCurrentDice] = useState(1);
 
-      <p>Click on Dice to Roll</p>
-    </div>
+  const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const rollDice = () => {
+    const randomNumber = generateRandomNumber(1, 7);
+    setCurrentDice((prev) => randomNumber);
+  };
+
+  console.log(generateRandomNumber);
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={rollDice}>
+        <img src={`images/dice_${currentDice}.png`} alt="Dice 1" srcset="" />
+
+        <p>Click on Dice to Roll</p>
+      </div>
+    </DiceContainer>
   );
 };
 
 export default RollDice;
+
+const DiceContainer = styled.div`
+  cursor: pointer;
+`;

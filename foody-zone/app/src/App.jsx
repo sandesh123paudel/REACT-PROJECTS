da8d8f74a026d0f34daff0afba944ef29ccf99c6
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-30 09:01:03 +0545

    Initialize Foody Zone project structure with Vite and React; remove unused files and add ESLint configuration

---
+++ foody-zone/app/src/App.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <MainContainer>
+      <TopContainer>
+        <div className="logo">
+          <img src="/Foody Zone.svg" alt="Logo" />
+        </div>
+
+        <div className="search">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Search Food Items....."
+          />
+        </div>
+      </TopContainer>
+    </MainContainer>
+  );
+};
+
+export default App;
+
+const MainContainer = styled.div`
+  background-color: black;
+  color: red;
+`;
+
+const TopContainer = styled.section``;

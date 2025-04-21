1e874dffa8f33d3ca086f8e0ffb2c7f587231976
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-04-21 20:43:09 +0545

    feat: add Sidebar component with navigation links and icons

--- crypto-dashboard/src/components/SideBar.jsx
+++ crypto-dashboard/src/components/SideBar.jsx
@@ -1,8 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { HStack, Icon, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import { RxDashboard, RxDoubleArrowRight } from "react-icons/rx";
 
 const SideBar = () => {
-  return <Box as="aside">This is Sidebar</Box>;
+  const navLinks = [
+    {
+      icon: RxDashboard,
+      text: "Dashboard",
+      url: "/",
+    },
+    {
+      icon: RxDoubleArrowRight,
+      text: "Transactions",
+      url: "/transactions",
+    },
+  ];
+
+  return (
+    <Stack boxShadow="sm">
+      {navLinks.map((nav) => (
+        <HStack key={nav.name}>
+          <Icon as={nav.icon}></Icon>
+          <Text as="h1">{nav.text}</Text>
+        </HStack>
+      ))}
+    </Stack>
+  );
 };
 
 export default SideBar;

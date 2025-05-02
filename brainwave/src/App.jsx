cf9b7579d4e079b81add49ffa6ee0e4ff6750556
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-05-02 08:58:27 +0545

    feat: refactor App component and add Header and Button components

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25redm] overflow-hidden ">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
   );
+  s;
 };
 
 export default App;

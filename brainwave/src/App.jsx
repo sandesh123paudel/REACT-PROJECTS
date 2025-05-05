7143f88459f4852ad18f5860db9d2039d1df8e7b
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-05-05 10:17:20 +0545

    feat: enhance Header component and add new constants for navigation and benefits

--- brainwave/src/App.jsx
+++ brainwave/src/App.jsx
@@ -16,3 +16,22 @@ const App = () => {
 };
 
 export default App;
+
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25redm] overflow-hidden ">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+  s;
+};
+
+export default App;

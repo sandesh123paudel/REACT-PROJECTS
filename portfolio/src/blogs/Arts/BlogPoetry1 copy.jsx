cd658afbaf718e75ad04864c3b030323b3f8973b
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-08-03 22:53:50 +0545

    New Content Updated

---
+++ portfolio/src/blogs/Arts/BlogPoetry1 copy.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const BlogPoetry1 = () => {
+  return (
+    <div className="prose lg:prose-xl max-w-none text-[ #d1953b]">
+      <p>
+        Poetry is where the soul speaks in whispers too profound for ordinary
+        conversation...
+      </p>
+      <ul>
+        <li>
+          <strong>Compression</strong>: Saying more with less
+        </li>
+        <li>
+          <strong>Musicality</strong>: Playing with sound
+        </li>
+      </ul>
+      <blockquote>
+        “Poetry is when an emotion has found its thought and the thought has
+        found words.” – Robert Frost
+      </blockquote>
+    </div>
+  );
+};
+
+export default BlogPoetry1;
